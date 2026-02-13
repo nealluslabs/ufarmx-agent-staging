456f98e7f17c6da0ec33f13f936e885d78c21284
Dagogo-Clinton-Uranta <dagogouranta@gmail.com>
2026-02-13 19:18:22 +0100

    new staging branch

--- frontend/src/redux/actions/baseUrl.js
+++ frontend/src/redux/actions/baseUrl.js
@@ -1,5 +1,5 @@
 
- const baseUrl = 'https://ufarmx-agent-server.vercel.app' 
+ const baseUrl = 'https://ufarmx-agent-server.vercel.app'  
  //const baseUrl = 'https://ufarmx-retailer-server-standalone.vercel.app'
 
 //const baseUrl = 'http://localhost:5000'
